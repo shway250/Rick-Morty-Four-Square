0e1f870212123726f7285890e055a12d45f4f6fe
shway250 <jackson_duhon@yahoo.com>
2016-08-03 09:33:16 -0700

    finishing up gravity function

--- project_1/script.js
+++ project_1/script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 var playerOne = 1;
-
 var playCircles = $('.playCircle');
 
 
@@ -12,16 +11,34 @@ var row3 = $('div.row3');
 var row4 = $('div.row4');
 var row5 = $('div.row5');
 var row6 = $('div.row6');
-
 var gameBoardArray = [row1, row2, row3, row4, row5, row6];
-
+ 
 
 playCircles.click(function(event){
+
+  var parentGameBoard = $(event.target).parents();
+  var clickedDiv = $(this);
+  var indexClickedDiv = clickedDiv.index();
+
+
+
+  //console.log(parentGameBoard[1].children[10], '6th row');///this prints row 6
+  //console.log(row1.children().eq(3).index()); //prints out proper index
+  //console.log($(this).index(), 'trying this stuff'); //proper index
+  //console.log(gameBoardArray[5][0].children[indexClickedDiv]);//fucking money!!!!!
+  console.log($(gameBoardArray[5][0].children[indexClickedDiv]).css('background-color'), "black and junk");
+
+
+//&& event.target.style.backgroundColor != 'yellow'=>logic for not double clicking
+
   if(playerOne){
-    event.target.style.backgroundColor = 'red';
-   checkWinner(gameBoardArray);
-   announceWinner();
-   playerOne= playerOne - 1;
+    if($(gameBoardArray[5][0].children[indexClickedDiv]).css('background-color') == 'rgb(0,194,214)'){////THis worked when RGB was black. keep at it
+      $(gameBoardArray[5][0].children[indexClickedDiv]).css('background-color', 'rgb(255, 0, 0)');
+      console.log($(gameBoardArray[5][0].children[indexClickedDiv]).css('background-color'), "red");
+      checkWinner(gameBoardArray);
+      announceWinner();
+      playerOne= playerOne - 1;
+    }
   }else{
     event.target.style.backgroundColor = 'yellow';
    checkWinner(gameBoardArray);
@@ -29,6 +46,10 @@ playCircles.click(function(event){
    playerOne= playerOne + 1;
   }
 });
+
+
+
+
 
 function checkWinner(checker){
   /////Checks rows for winner
