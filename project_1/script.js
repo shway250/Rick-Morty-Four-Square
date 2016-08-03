326af960d86243b83386517c12dac0657609a574
shway250 <jackson_duhon@yahoo.com>
2016-08-03 12:00:40 -0700

    logic done, starting on design and animation

--- project_1/script.js
+++ project_1/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 var playerOne = 1;
 var playCircles = $('.playCircle');
-
+var reset = $('#button');
 
 ///////////////////////////Row Variables///////////////
 var row1 = $('div.row1');
@@ -13,53 +13,118 @@ var row5 = $('div.row5');
 var row6 = $('div.row6');
 var gameBoardArray = [row1, row2, row3, row4, row5, row6];
  
+reset.click(function(event){
+  for(var i = 0; i < 6; i++){
+    for(var j = 0; j< 7; j++){
+      gameBoardArray[i][0].children[j].style.backgroundColor = 'rgb(0, 194, 214)';
+    }
+  }
+  console.log('reset button clicked');
+});
 
 playCircles.click(function(event){
 
-  var parentGameBoard = $(event.target).parents();
+  ///////var parentGameBoard = $(event.target).parents();
+  ///finding clicked div
   var clickedDiv = $(this);
   var indexClickedDiv = clickedDiv.index();
-
-
-
-  //console.log(parentGameBoard[1].children[10], '6th row');///this prints row 6
-  //console.log(row1.children().eq(3).index()); //prints out proper index
-  //console.log($(this).index(), 'trying this stuff'); //proper index
-  //console.log(gameBoardArray[5][0].children[indexClickedDiv]);//fucking money!!!!!
-  console.log($(gameBoardArray[5][0].children[indexClickedDiv]).css('background-color'), "black and junk");
-
-
-//&& event.target.style.backgroundColor != 'yellow'=>logic for not double clicking
+  ////Available play div variables
+  var firstRowToken = $(gameBoardArray[0][0].children[indexClickedDiv]);
+  var secondRowToken = $(gameBoardArray[1][0].children[indexClickedDiv]);
+  var thirdRowToken = $(gameBoardArray[2][0].children[indexClickedDiv]);
+  var fourthRowToken = $(gameBoardArray[3][0].children[indexClickedDiv]);
+  var fifthRowToken = $(gameBoardArray[4][0].children[indexClickedDiv]);
+  var sixthRowToken = $(gameBoardArray[5][0].children[indexClickedDiv]);
 
   if(playerOne){
-    if($(gameBoardArray[5][0].children[indexClickedDiv]).css('background-color') == 'rgb(0,194,214)'){////THis worked when RGB was black. keep at it
-      $(gameBoardArray[5][0].children[indexClickedDiv]).css('background-color', 'rgb(255, 0, 0)');
-      console.log($(gameBoardArray[5][0].children[indexClickedDiv]).css('background-color'), "red");
+    //getting this far
+    if(sixthRowToken.css('background-color') == 'rgb(0, 194, 214)'){
+      $(sixthRowToken.css('background-color', 'red'));
       checkWinner(gameBoardArray);
       announceWinner();
       playerOne= playerOne - 1;
     }
-  }else{
-    event.target.style.backgroundColor = 'yellow';
-   checkWinner(gameBoardArray);
-   announceWinner();
-   playerOne= playerOne + 1;
+    else if(fifthRowToken.css('background-color') == 'rgb(0, 194, 214)'){
+      $(fifthRowToken.css('background-color', 'red'));
+      checkWinner(gameBoardArray);
+      announceWinner();
+      playerOne= playerOne - 1;
+    }
+    else if(fourthRowToken.css('background-color') == 'rgb(0, 194, 214)'){
+      $(fourthRowToken.css('background-color', 'red'));
+      checkWinner(gameBoardArray);
+      announceWinner();
+      playerOne= playerOne - 1;
+    }
+    else if(thirdRowToken.css('background-color') == 'rgb(0, 194, 214)'){
+      $(thirdRowToken.css('background-color', 'red'));
+      checkWinner(gameBoardArray);
+      announceWinner();
+      playerOne= playerOne - 1;
+    }
+    else if(secondRowToken.css('background-color') == 'rgb(0, 194, 214)'){
+      $(secondRowToken.css('background-color', 'red'));
+      checkWinner(gameBoardArray);
+      announceWinner();
+      playerOne= playerOne - 1;
+    }
+    else if(firstRowToken.css('background-color') == 'rgb(0, 194, 214)'){
+      $(firstRowToken.css('background-color', 'red'));
+      checkWinner(gameBoardArray);
+      announceWinner();
+      playerOne= playerOne - 1;
+    }
+  }
+  /////////////======player two logic===========/////////////////
+  else if (!playerOne){
+    if(sixthRowToken.css('background-color') == 'rgb(0, 194, 214)'){
+      $(sixthRowToken.css('background-color', 'yellow'));
+      checkWinner(gameBoardArray);
+      announceWinner();
+      playerOne= playerOne + 1;
+    }
+    else if(fifthRowToken.css('background-color') == 'rgb(0, 194, 214)'){
+      $(fifthRowToken.css('background-color', 'yellow'));
+      checkWinner(gameBoardArray);
+      announceWinner();
+      playerOne= playerOne + 1;
+    }
+    else if(fourthRowToken.css('background-color') == 'rgb(0, 194, 214)'){
+      $(fourthRowToken.css('background-color', 'yellow'));
+      checkWinner(gameBoardArray);
+      announceWinner();
+      playerOne= playerOne + 1;
+    }
+    else if(thirdRowToken.css('background-color') == 'rgb(0, 194, 214)'){
+      $(thirdRowToken.css('background-color', 'yellow'));
+      checkWinner(gameBoardArray);
+      announceWinner();
+      playerOne= playerOne + 1;
+    }
+    else if(secondRowToken.css('background-color') == 'rgb(0, 194, 214)'){
+      $(secondRowToken.css('background-color', 'yellow'));
+      checkWinner(gameBoardArray);
+      announceWinner();
+      playerOne= playerOne + 1;
+    }
+    else if(firstRowToken.css('background-color') == 'rgb(0, 194, 214)'){
+      $(firstRowToken.css('background-color', 'yellow'));
+      checkWinner(gameBoardArray);
+      announceWinner();
+      playerOne= playerOne + 1;
+    }
   }
 });
 
-
-
-
-
 function checkWinner(checker){
-  /////Checks rows for winner
+  ///////////////Checks rows for winner
   for(var i = 0; i < 6; i++){
     for(var j = 0; j < 4; j++){
       if(gameBoardArray[i][0].children[j].style.backgroundColor == "red" &&
         gameBoardArray[i][0].children[j+1].style.backgroundColor == "red" &&
         gameBoardArray[i][0].children[j+2].style.backgroundColor == "red" &&
         gameBoardArray[i][0].children[j+3].style.backgroundColor == "red"){
-        console.log('function works');
+        console.log('red wins');
       return true;
       }
     }
@@ -70,8 +135,71 @@ function checkWinner(checker){
         gameBoardArray[i][0].children[j+1].style.backgroundColor == "yellow" &&
         gameBoardArray[i][0].children[j+2].style.backgroundColor == "yellow" &&
         gameBoardArray[i][0].children[j+3].style.backgroundColor == "yellow"){
-        console.log('function works');
+        console.log('yellow wins');
       return true;
+      }
+    }
+  }
+  //////////////Check Columns for winner
+  for(var i = 0; i < 3; i++){
+    for(var j = 0; j< 7; j++){
+      if(gameBoardArray[i][0].children[j].style.backgroundColor == "red" &&
+        gameBoardArray[i+1][0].children[j].style.backgroundColor == "red" &&
+        gameBoardArray[i+2][0].children[j].style.backgroundColor == "red" &&
+        gameBoardArray[i+3][0].children[j].style.backgroundColor == "red"){
+        console.log('red wins')
+      }
+    }
+  }
+  for(var i = 0; i < 3; i++){
+    for(var j = 0; j< 7; j++){
+      if(gameBoardArray[i][0].children[j].style.backgroundColor == "yellow" &&
+        gameBoardArray[i+1][0].children[j].style.backgroundColor == "yellow" &&
+        gameBoardArray[i+2][0].children[j].style.backgroundColor == "yellow" &&
+        gameBoardArray[i+3][0].children[j].style.backgroundColor == "yellow"){
+        console.log('yellow wins')
+      }
+    }
+  }
+  ////////////////Checking down right for winner
+for(var i = 0; i < 3; i++){
+    for(var j = 0; j< 4; j++){
+      if(gameBoardArray[i][0].children[j].style.backgroundColor == "red" &&
+        gameBoardArray[i+1][0].children[j+1].style.backgroundColor == "red" &&
+        gameBoardArray[i+2][0].children[j+2].style.backgroundColor == "red" &&
+        gameBoardArray[i+3][0].children[j+3].style.backgroundColor == "red"){
+        console.log('red wins')
+      }
+    }
+  }
+  for(var i = 0; i < 3; i++){
+    for(var j = 0; j< 4; j++){
+      if(gameBoardArray[i][0].children[j].style.backgroundColor == "yellow" &&
+        gameBoardArray[i+1][0].children[j+1].style.backgroundColor == "yellow" &&
+        gameBoardArray[i+2][0].children[j+2].style.backgroundColor == "yellow" &&
+        gameBoardArray[i+3][0].children[j+3].style.backgroundColor == "yellow"){
+        console.log('yellow wins')
+      }
+    }
+  }
+  ///////////////////////checking down left for winner
+  for(var i = 3; i < 6; i++){
+    for(var j = 0; j< 4; j++){
+      if(gameBoardArray[i][0].children[j].style.backgroundColor == "red" &&
+        gameBoardArray[i-1][0].children[j+1].style.backgroundColor == "red" &&
+        gameBoardArray[i-2][0].children[j+2].style.backgroundColor == "red" &&
+        gameBoardArray[i-3][0].children[j+3].style.backgroundColor == "red"){
+        console.log('red wins')
+      }
+    }
+  }
+  for(var i = 3; i < 6; i++){
+    for(var j = 0; j< 4; j++){
+      if(gameBoardArray[i][0].children[j].style.backgroundColor == "yellow" &&
+        gameBoardArray[i-1][0].children[j+1].style.backgroundColor == "yellow" &&
+        gameBoardArray[i-2][0].children[j+2].style.backgroundColor == "yellow" &&
+        gameBoardArray[i-3][0].children[j+3].style.backgroundColor == "yellow"){
+        console.log('yellow wins')
       }
     }
   }
@@ -84,6 +212,17 @@ function announceWinner(){
     console.log('player two wins')
   }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
